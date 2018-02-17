a0c1bfb0777e1c18c5e20a7ba1d0a4182e9fba78
CarlosRDGZ <crodriguez20@ucol.mx>
2018-02-17 12:07:20 -0600

    Ejercicios 1-3. 17-2-2018

---
+++ Tarea1_Prueba_TStudent/source/TStudent.R
@@ -0,0 +1,27 @@
+# Values 1
+# x <- c(20, 30, 25, 20, 30, 50, 25, 20, 20, 30, 25, 40, 50, 35, 20)
+# mu0 <- 25
+
+# Values 2
+# x <- c(5, 2, 9, 7, 4, 5, 6, 7, 7, 10, 5, 6, 5, 4, 5, 8, 6, 6, 3, 6, 7, 3, 6)
+# mu0 <- 8
+
+# Values 3
+x <- c(61, 64, 68, 67, 70, 73, 76, 78, 81, 67, 70, 73, 66, 74, 77, 80, 85, 75, 72, 78, 77, 80)
+mu0 <- 75
+
+# mean
+x.mean <- mean(x)
+n <- length(x)
+
+# variance
+sigma2 <- ( sum(x ^ 2) - (n * x.mean ^ 2) ) / (n - 1)
+
+# standard deviation
+sigma_x <- sqrt(sigma2 / n)
+
+# tcal
+t_cal <- (x.mean - mu0) / sigma_x
+
+# ttablas
+t_tab <- qt(0.05/2, df = n, lower.tail = F)
