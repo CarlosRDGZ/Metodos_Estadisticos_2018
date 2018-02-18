0e112d8f1f3c5331174995ca63a75783f9a284d5
CarlosRDGZ <crodriguez20@ucol.mx>
2018-02-18 00:20:32 -0600

    tStudent. Two Sample. 18-2-2018

---
+++ Tarea1_Prueba_TStudent/source/TwoIndepTStudent.R
@@ -0,0 +1,64 @@
+# Values 1
+# x <- c(35, 23, 20, 25, 12, 17, 24, 22, 20, 28, 32, 30, 15, 19, 26)
+# y <- c(32, 27, 23, 30, 15, 20, 21, 25, 23, 25, 29, 27, 18, 22, 29)
+
+# Values 2
+# x <- c(29, 10, 27, 8, 26, 11, 25, 7, 13, 9, 28, 24, 7, 22, 9)
+# y <- c(9, 14, 11, 8, 15, 19, 21, 13, 10, 8, 17, 22, 19, 11, 7)
+
+# Values 3
+x <- c(17.6211, 16.6897, 15.4212, 14.5710,
+       16.0258, 15.5761, 14.1005, 16.4496,
+       15.1608, 16.3500, 15.9580, 18.2257,
+       15.7624, 16.5477, 16.4090)
+y <- c(11.1581, 8.3344, 14.9567, 7.8432,
+       10.9840, 11.2624, 13.3290, 8.8406,
+       8.5133, 11.0786, 5.7472, 9.3190,
+       14.1809, 10.9265, 12.6853)
+
+# Operations
+
+# mean x
+x.mean <- mean(x)
+n_x    <- length(x)
+# variance x
+S2.x   <- ( sum(x ^ 2) - (n_x * x.mean ^ 2) ) / (n_x - 1)
+
+# mean y
+y.mean <- mean(y)
+n_y    <- length(y)
+# variance y
+S2.y   <- ( sum(y ^ 2) - (n_y * y.mean ^ 2) ) / (n_y - 1)
+
+mu0   <- 0
+# Varianza Poblacional
+S_p   <- sqrt( (((n_x - 1) * S2.x) + ((n_y - 1) * S2.y)) / (n_x + n_y - 2) )
+# tCal
+t_cal <- (x.mean - y.mean - mu0) / (S_p * sqrt((1/n_x) + (1/n_y)))
+# tTablas
+t_tab <- qt(0.5/2, df = n_x + n_y - 2, lower.tail = F)
+
+# Desicion de Hipotesis
+abs(t_cal) > t_tab
+
+# t test
+data  <- c(x, y)
+grups <- rep(c(1,2), each = n_x)
+test  <- t.test(data~grups, na.rm = T, mu = 0, alternative = "two.sided", var.equal = T, conf.level = 0.95)
+
+# Supuesto Normalidad
+qqnorm(data)
+qqline(data)
+shapiro.test(data)
+
+x.mean
+S2.x
+y.mean
+S2.y
+
+S_p
+t_cal
+t_tab
+
+test
+
--- Tarea1_Prueba_TStudent/source/js/Tarea1.js
+++ Tarea1_Prueba_TStudent/source/js/Tarea1.js
@@ -34,8 +34,82 @@ var data = {
 }
 */
 
+/* Ejercicio 4
+var data = {
+    x: {
+        values: [35, 23, 20, 25, 12, 17, 24, 22, 20, 28, 32, 30, 15, 19, 26],
+        mean: 0,
+        vrnc: 0,
+    },
+    y: {
+        values: [32, 27, 23, 30, 15, 20, 21, 25, 23, 25, 29, 27, 18, 22, 29],
+        mean: 0,
+        vrnc: 0,
+    },
+    mu0: 0,
+    stdD: 0,
+    tCal: 0
+}
+*/
+
+/* Ejercicio 5
+var data = {
+    x: {
+        values: [ 29, 10, 27, 8, 26, 11, 25, 7, 13, 9, 28, 24, 7, 22, 9 ],
+        mean: 0,
+        vrnc: 0,
+    },
+    y: {
+        values: [ 9, 14, 11, 8, 15, 19, 21, 13, 10, 8, 17, 22, 19, 11, 7 ],
+        mean: 0,
+        vrnc: 0,
+    },
+    mu0: 0,
+    stdD: 0,
+    tCal: 0
+}
+*/
+
+/* Ejercicio 6 */
+var data = {
+    x: {
+        values: [17.6211, 16.6897, 15.4212, 14.5710,
+            16.0258, 15.5761, 14.1005, 16.4496,
+            15.1608, 16.3500, 15.9580, 18.2257,
+            15.7624, 16.5477, 16.4090],
+        mean: 0,
+        vrnc: 0,
+    },
+    y: {
+        values: [11.1581, 8.3344, 14.9567, 7.8432,
+            10.9840, 11.2624, 13.3290, 8.8406,
+            8.5133, 11.0786, 5.7472, 9.3190,
+            14.1809, 10.9265, 12.6853],
+        mean: 0,
+        vrnc: 0,
+    },
+    mu0: 0,
+    stdD: 0,
+    tCal: 0
+}
+
+/* tStudent 2 Indipendent Samples */
+console.log('xMean:', latex.mean(data.x))
+console.log('yMean:', latex.mean(data.y, 'y'))
+console.log('xVrnc:', latex.variance(data.x,'x','S'))
+console.log('yVrnc:', latex.variance(data.y,'y','S'))
+console.log('stdD:', latex.stdDesv(data, 'twoind'))
+console.log('tCal:', latex.tCal(data, 'twoind'))
+for(const prop in data) {
+    console.log(`${prop}[mean]: ${data[prop].mean}`)
+    console.log(`${prop}[variance]: ${data[prop].vrnc}`)
+}
+console.log(`stdD: ${data.stdD}`)
+console.log(`tCal: ${data.tCal}`)
+
+/* tStuden One Sample
 console.log(latex.mean(data))
-console.log(latex.varianza(data))
+console.log(latex.variance(data))
 console.log(latex.stdDesv(data))
 console.log(latex.tCal(data))
 
@@ -43,3 +117,4 @@ console.log('')
 for(const prop in data) {
     console.log(`${prop}: ${data[prop]}`)
 }
+*/
