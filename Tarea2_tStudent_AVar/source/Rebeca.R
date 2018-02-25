daaf0ee9b13902a09407858f18994e041727407d
CarlosRDGZ <crodriguez20@ucol.mx>
2018-02-25 15:46:07 -0600

    AOV class and Latex. JS

--- Tarea2_tStudent_AVar/source/Rebeca.R
+++ Tarea2_tStudent_AVar/source/Rebeca.R
@@ -1,8 +1,16 @@
-# Values 1
-tratamiento1 <- c(22, 28, 26, 18, 20, 19, 15, 20, 23, 21, 19, 20, 19, 18, 21)
-tratamiento2 <- c(26, 30, 24, 22, 21, 21, 18, 19, 21, 19, 18, 19, 17, 18, 22)
+# Ejerccio 1
+# tratamiento1 <- c(22, 28, 26, 18, 20, 19, 15, 20, 23, 21, 19, 20, 19, 18, 21)
+# tratamiento2 <- c(26, 30, 24, 22, 21, 21, 18, 19, 21, 19, 18, 19, 17, 18, 22)
 
-diferencia <- tratamiento1 - tratamiento2
+# Ejerccio 2
+# tratamiento1 <- c(4, 4.5, 5, 3, 4.5, 5, 6, 4, 5, 6)
+# tratamiento2 <- c(3, 5, 4, 3.3, 4, 4, 5, 4, 3, 5)
+
+# Ejercicio 3
+LIA <- c(88, 80, 90, 88, 92, 89, 98, 94, 91, 95, 90, 94)
+LFA <- c(70, 77, 85, 84, 80, 88, 94, 90, 89, 99, 93, 93)
+
+diferencia <- LIA - LFA
 valor.esperado   <- 0
 
 # promedio de la diferencia entre los tratamientos
@@ -25,7 +33,7 @@ tCal <- (promedio - valor.esperado) / error.std
 tCal
 
 # tTablas
-tTablas <- qt(0.1/2, df = n - 1, lower.tail = F)
+tTablas <- qt(0.01/2, df = n - 1, lower.tail = F)
 tTablas
 
 # Desicion de Hipotesis
@@ -33,7 +41,7 @@ abs(tCal) > tTablas
 
 # t test
 # Muestras
-respuesta <- c(tratamiento1, tratamiento2)
+respuesta <- c(LIA, LFA)
 
 grupos <- rep(c(1,2), each = n)
 t.test1  <- t.test(respuesta~grupos, na.rm = T, mu = 0, alternative = "two.sided", paired = T, conf.level = 0.10)
