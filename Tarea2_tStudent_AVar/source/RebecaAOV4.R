046612527ea61807b0f5e9534b5f59cdb606923f
CarlosRDGZ <crodriguez20@ucol.mx>
2018-02-26 00:01:57 -0600

    2-26-2018

--- Tarea2_tStudent_AVar/source/RebecaAOV4.R
+++ Tarea2_tStudent_AVar/source/RebecaAOV4.R
@@ -1,17 +1,17 @@
-linea1 <- c(452,379,412,320,350,390,378)
-linea2 <- c(322,345,367,341,372,317,324)
-linea3 <- c(298,312,280,310,235,304,320)
-linea4 <- c(340,358,345,362,370,323,368)
+line1 <- c(452,379,412,320,350,390,378)
+line2 <- c(322,345,367,341,372,317,324)
+line3 <- c(298,312,280,310,235,304,320)
+line4 <- c(340,358,345,362,370,323,368)
 
-lineas <- data.frame(linea1,linea2,linea3,linea4)
+lines <- data.frame(line1,line2,linea3,line4)
 
-apply(lineas, 2, sum)
-apply(lineas, 2, mean)
+apply(lines, 2, sum)
+apply(lines, 2, mean)
 
-nLineas <- as.factor(rep(c(1, 2, 3, 4), each = 7))
-lineas <- data.frame(nLineas, linea=c(linea1,linea2,linea3,linea4))
+nLines <- as.factor(rep(c(1, 2, 3, 4), each = 7))
+lines <- data.frame(nLines, linea=c(line1,line2,linea3,line4))
 
-modelo <- aov(linea ~ nLineas, data=lineas)
+modelo <- aov(linea ~ nLines, data=lines)
 
 summary(modelo)
 
@@ -20,9 +20,16 @@ TukeyHSD(modelo)
 plot(TukeyHSD(modelo), las=1, col="brown")
 
 library(agricolae)
-Prueba.HSD <- HSD.test(modelo, "nLineas", group=TRUE)
+Prueba.HSD <- HSD.test(modelo, "nLines", group=TRUE)
 Prueba.HSD$groups
 
 qqnorm(modelo$residuals) 
 qqline(modelo$residuals)
 shapiro.test(modelo$residuals)
+
+> apply(lines, 2, sum)
+ line1  line2 linea3  line4 
+  2681   2388   2059   2466 
+> apply(lines, 2, mean)
+   line1    line2   linea3    line4 
+383.0000 341.1429 294.1429 352.2857 
--- Tarea2_tStudent_AVar/source/RebecaAOV7.R
+++ Tarea2_tStudent_AVar/source/RebecaAOV7.R
@@ -1,16 +1,16 @@
-marca1 <- c(1.04,1.05,1.06,1.07,1.05)
-marca2 <- c(1.10,1.11,1.09,1.07,1.08)
-marca3 <- c(1.21,1.19,1.18,1.17,1.21)
+solucion1 <- c(13,22,18)
+solucion2 <- c(16,24,17)
+solucion3 <- c(5,4,1)
 
-marcas <- data.frame(marca1,marca2,marca3)
+soluciones <- data.frame(solucion1,solucion2,solucion3)
 
-apply(marcas, 2, sum)
-apply(marcas, 2, mean)
+apply(soluciones, 2, sum)
+apply(soluciones, 2, mean)
 
-nMarcas <- as.factor(rep(c(1, 2, 3), each = 5))
-marcas <- data.frame(nMarcas, marca=c(marca1,marca2,marca3))
+nSoluciones <- as.factor(rep(c(1, 2, 3), each = 3))
+soluciones <- data.frame(nSoluciones, solucion=c(solucion1,solucion2,solucion3))
 
-modelo <- aov(marca ~ nMarcas, data=marcas)
+modelo <- aov(solucion ~ nSoluciones, data=soluciones)
 
 summary(modelo)
 
@@ -19,7 +19,7 @@ TukeyHSD(modelo)
 plot(TukeyHSD(modelo), las=1, col="brown")
 
 library(agricolae)
-Prueba.HSD <- HSD.test(modelo, "nMarcas", group=TRUE)
+Prueba.HSD <- HSD.test(modelo, "nSoluciones", group=TRUE)
 Prueba.HSD$groups
 
 qqnorm(modelo$residuals) 
